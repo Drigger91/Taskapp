b4e98601b736e51a4e047c76a941f82a050bf140
Drigger91 <82764734+Drigger91@users.noreply.github.com>
2021-11-03 23:55:30 +0530

    fifth

--- src/index.js
+++ src/index.js
@@ -38,8 +38,7 @@ if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, '/client/build')));
 
     app.get('*', (req, res) => {
-    res.sendfile(path.join((__dirname, '/client/build/index.html')));
-    })
+    res.sendFile(path.join((__dirname, '/client/build/index.html')));
     })
 }
 //server call
