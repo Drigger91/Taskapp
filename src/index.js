662016a187b32f23f56008a8103603642c45abcf
Drigger91 <82764734+Drigger91@users.noreply.github.com>
2021-11-04 22:16:02 +0530

    koshishpart38834

--- src/index.js
+++ src/index.js
@@ -35,10 +35,10 @@ app.use(TaskRouter)
 
 
 //heroku
-app.use(express.static(path.join(__dirname, './client/build')))
+app.use(express.static(path.join(__dirname, '/client/build')))
 
 app.get('*', function(_, res) {
-  res.sendFile(path.join(__dirname, './client/build/index.html'), function(err) {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'), function(err) {
     if (err) {
       res.status(500).send(err)
     }
