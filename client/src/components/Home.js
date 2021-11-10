2f9393d346180b243aa72b330d74fba9243fba9d
Drigger91 <82764734+Drigger91@users.noreply.github.com>
2021-11-10 11:32:55 +0530

    loading spinner

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { NavLink, useHistory } from "react-router-dom";
 import Tasks from "./Tasks";
+import * as ReactBootstrap from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 function Home() {
   const history = useHistory();
 
   const [User, setUser] = useState({});
+  const [loading, setloading] = useState(false);
   const [display, setdisplay] = useState("home-container");
   const [display2, setdisplay2] = useState("d-none");
   const callPage = async () => {
@@ -17,11 +21,12 @@ function Home() {
         },
         credentials: "include",
       });
+      setloading(true);
       const data = await res.json();
-      setdisplay("d-none");
       setUser(data);
 
       if (res.status === 200) {
+        setdisplay("d-none");
         setdisplay2("main-container");
       }
     } catch (e) {
@@ -50,56 +55,62 @@ function Home() {
 
   return (
     <>
-      <div className={display}>
-        <div className="home-animation">
-          {" "}
-          <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-          <lottie-player
-            src="https://assets5.lottiefiles.com/packages/lf20_puciaact.json"
-            background="transparent"
-            speed=".5"
-            loop
-            autoplay
-          ></lottie-player>{" "}
-          <br />
-        </div>
-        <div>
-          <form className="text-align-center" method="POST">
-            <h2>
-              Welcome to <b id="nav-logo">Task-App!</b>
-            </h2>
+      {loading ? (
+        <>
+          <div className={display}>
+            <div className="home-animation">
+              {" "}
+              <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+              <lottie-player
+                src="https://assets5.lottiefiles.com/packages/lf20_puciaact.json"
+                background="transparent"
+                speed=".5"
+                loop
+                autoplay
+              ></lottie-player>{" "}
+              <br />
+            </div>
+            <div>
+              <form className="text-align-center" method="POST">
+                <h2>
+                  Welcome to <b id="nav-logo">Task-App!</b>
+                </h2>
 
-            <div className="mb-3">
-              <h3>
-                If you are new SignUp here -{" "}
-                <NavLink className="nav-link active" to="/SignUp">
-                  <button className="btn btn-primary">Signup</button>
-                </NavLink>
-              </h3>
+                <div className="mb-3">
+                  <h3>
+                    If you are new SignUp here -{" "}
+                    <NavLink className="nav-link active" to="/SignUp">
+                      <button className="btn btn-primary">Signup</button>
+                    </NavLink>
+                  </h3>
+                </div>
+                <div className="mb-3">
+                  <h3>
+                    If already a User Login here -{" "}
+                    <NavLink className="nav-link active" to="/login">
+                      <button className="btn btn-primary">Login</button>
+                    </NavLink>
+                  </h3>
+                </div>
+              </form>
             </div>
-            <div className="mb-3">
-              <h3>
-                If already a User Login here -{" "}
-                <NavLink className="nav-link active" to="/login">
-                  <button className="btn btn-primary">Login</button>
-                </NavLink>
-              </h3>
+          </div>
+          <div className={display2}>
+            <h1>
+              Welcome <span className="user-heading"> {User.name}</span>
+            </h1>
+            <button className="btn btn-primary" onClick={LogOut}>
+              {" "}
+              LogOut
+            </button>
+            <div className="task-ui">
+              <Tasks owner={User._id}></Tasks>
             </div>
-          </form>
-        </div>
-      </div>
-      <div className={display2}>
-        <h1>
-          Welcome <span className="user-heading"> {User.name}</span>
-        </h1>
-        <button className="btn btn-primary" onClick={LogOut}>
-          {" "}
-          LogOut
-        </button>
-        <div className="task-ui">
-          <Tasks owner={User._id}></Tasks>
-        </div>
-      </div>
+          </div>{" "}
+        </>
+      ) : (
+        <ReactBootstrap.Spinner animation="border" />
+      )}
     </>
   );
 }
