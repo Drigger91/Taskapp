f7b038a21d8d5d8035c3e77c9346abf8df9dfb20
Drigger91 <82764734+Drigger91@users.noreply.github.com>
2021-11-10 11:36:12 +0530

    loading part 2

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -21,12 +21,12 @@ function Home() {
         },
         credentials: "include",
       });
+      setdisplay("d-none");
       setloading(true);
       const data = await res.json();
       setUser(data);
 
       if (res.status === 200) {
-        setdisplay("d-none");
         setdisplay2("main-container");
       }
     } catch (e) {
